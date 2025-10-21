4e8699425e65604d407554aa7150be43763e6882
kobryant8 <gudwns9947@gmail.com>
2025-10-21 16:33:16 +0900

    Add files via upload

---
+++ portfolio-data.js
@@ -0,0 +1,77 @@
+/*
+ * Portfolio data definition
+ *
+ * This file defines an array of portfolio items used by the dynamic
+ * portfolio carousel. Each object in the `portfolioItems` array
+ * describes a project with a title, description, image path, and a URL.
+ * To add a new project to your portfolio, simply append another object
+ * to the array following the same structure. The carousel will
+ * automatically generate slides and navigation for any number of items.
+ */
+
+var portfolioItems = [
+  {
+    title: '프로젝트 1',
+    description: '첫 영상 편집 제작물입니다. 컷편집, 효과 등을 담당했습니다.',
+    image: 'assets/po1.png',
+    url: 'https://www.youtube.com/watch?v=5pmDldL8vX8'
+  },
+  {
+    title: '프로젝트 2',
+    description: '두번째 프로젝트 영상 편집입니다. 컷편집, 효과등을 담당했습니다.',
+    image: 'assets/po2.png',
+    url: 'https://www.youtube.com/watch?v=JzAXIvYE6tY'
+  },
+  {
+    title: '프로젝트 3',
+    description: '기획, 편집, 효과 제작을 한 광고 영상제작입니다. 학원에서 처음으로 만들어본 영상입니다.',
+    image: 'assets/po3.png',
+    url: 'https://www.youtube.com/watch?v=m4uCg7Df6CU'
+  },
+  {
+    title: '프로젝트 4',
+    description: '생성형 AI 제작 영상입니다. 미드저니 AI 툴을 이용했습니다.',
+    image: 'assets/po4.png',
+    url: 'https://www.youtube.com/watch?v=_nhsHzS4pwM'
+  },
+  {
+    title: '프로젝트 5',
+    description: '학원에서 팀원들과 제작한 타이포그래피 영상 입니다. 프로젝트에서 팀장, 기획, 파트3(영상 후반부)를 맡았습니다.',
+    image: 'assets/po5.png',
+    url: 'https://www.youtube.com/watch?v=zDhzu2kl76M'
+  },
+    {
+    title: '프로젝트 6',
+    description: '생성형 AI 제작 영상입니다. 미드저니 AI 툴을 이용했습니다.',
+    image: 'assets/po6.png',
+    url: 'https://www.youtube.com/watch?v=qc44fWQWUQY'
+  },
+    {
+    title: '프로젝트 7',
+    description: '생성형 AI 제작 영상입니다. 미드저니 AI 툴을 이용했습니다.',
+    image: 'assets/po7.png',
+    url: 'https://www.youtube.com/watch?v=B3Jr8WpAPAo'
+  },
+    {
+    title: '프로젝트 8',
+    description: '생성형 AI 제작 영상입니다. 미드저니 AI 툴을 이용했습니다.',
+    image: 'assets/po8.png',
+    url: 'https://www.youtube.com/watch?v=Jpo9D5d9IG4'
+  },  {
+    title: '프로젝트 9',
+    description: '생성형 AI 제작 영상입니다. 미드저니 AI 툴을 이용했습니다.',
+    image: 'assets/po9.png',
+    url: 'https://www.youtube.com/watch?v=JDNcqOB0DKw'
+  },  {
+    title: '프로젝트 10',
+    description: '생성형 AI 제작 영상입니다. 미드저니 AI 툴을 이용했습니다.',
+    image: 'assets/po10.png',
+    url: 'https://www.youtube.com/watch?v=si_8xcDC9Vg'
+  },
+    {
+    title: '프로젝트 11',
+    description: '생성형 AI 제작 영상입니다. 미드저니 AI 툴을 이용했습니다.',
+    image: 'assets/po11.png',
+    url: 'https://www.youtube.com/watch?v=XYNLVchIT18'
+  },
+];
